b623aa0f986fdc4d7bf790a9fce16a9e8dfdcaf4
Albina Vivdenko <albinka3322@gmail.com>
2025-01-29 19:53:49 +0100

    updata, delete create

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -10,44 +10,58 @@ export const getContactById = async (contactId) => {
   return contact;
 };
 
-export const createContact = async ({
-  name,
-  phoneNumber,
-  email,
-  isFavourite,
-  contactType,
-}) => {
-  const newContact = new ContactsCollection({
-    name,
-    phoneNumber,
-    email,
-    isFavourite,
-    contactType,
+// export const createContact = async ({
+//   name,
+//   phoneNumber,
+//   email,
+//   isFavourite,
+//   contactType,
+// }) => {
+//   const newContact = new ContactsCollection({
+//     name,
+//     phoneNumber,
+//     email,
+//     isFavourite,
+//     contactType,
+//   });
+
+//   await newContact.save();
+
+//   return newContact;
+// };
+
+export const createContact = async (payload) => {
+  return await ContactsCollection.create(payload);
+};
+
+export const updateContact = async (contactId, payload) => {
+  return await ContactsCollection.findByIdAndUpdate(contactId, payload, {
+    new: true,
   });
-
-  await newContact.save();
-
-  return newContact;
 };
 
-export const updateContact = async (contactId, updates) => {
-  const contact = await ContactsCollection.findById(contactId);
+// export const updateContact = async (contactId, updates) => {
+//   const contact = await ContactsCollection.findById(contactId);
 
-  if (!contact) {
-    throw { status: 404, message: 'Contact not found' };
-  }
+//   if (!contact) {
+//     throw { status: 404, message: 'Contact not found' };
+//   }
 
-  const updatedContact = await contact.updateOne(updates);
-  return updatedContact;
-};
+//   const updatedContact = await contact.updateOne(updates);
+//   return updatedContact;
+// };
+
+// export const deleteContact = async (contactId) => {
+//   const contact = await ContactsCollection.findById(contactId);
+
+//   if (!contact) {
+//     return null;
+//   }
+
+//   await contact.deleteOne();
+//   return contact;
+// };
 
 export const deleteContact = async (contactId) => {
-  const contact = await ContactsCollection.findById(contactId);
-
-  if (!contact) {
-    return null;
-  }
-
-  await contact.deleteOne();
-  return contact;
+  return await ContactsCollection.findOneAndDelete({ _id: contactId });
 };
