43455cae5acb0c3d763728742e67c2468c7974dd
Albina Vivdenko <albinka3322@gmail.com>
2025-01-30 17:47:56 +0100

    ff

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -8,13 +8,26 @@ import {
   deleteContact,
 } from '../services/contacts.js';
 
-export const getAllContactsController = async (req, res) => {
-  const allContacts = await getAllContacts();
-  res.json({
-    status: 200,
-    message: 'Successfully found contacts!',
-    data: allContacts,
-  });
+// export const getAllContactsController = async (req, res, next) => {
+//   const allContacts = await getAllContacts();
+//   res.json({
+//     status: 200,
+//     message: 'Successfully found contacts!',
+//     data: allContacts,
+//   });
+// };
+
+export const getAllContactsController = async (req, res, next) => {
+  try {
+    const allContacts = await getAllContacts();
+    res.json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: allContacts,
+    });
+  } catch (err) {
+    next(err); // Передаємо помилку в middleware
+  }
 };
 
 export const getContactByIdController = async (req, res, next) => {
